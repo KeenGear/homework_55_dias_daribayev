38e9e9e318e356a902aa0a8cfe0e6742748a75a5
Dias Daribayev <fission.frames-0s@icloud.com>
2023-02-16 12:11:33 +0600

    Changed task_list.html and urls.py

--- source/todoapp/urls.py
+++ source/todoapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import task_list, task_create, task_delete, task_update
+from .views import task_list, task_create, task_delete, task_update, task_view
 
 urlpatterns = [
     path('', task_list, name='task_list'),
     path('create/', task_create, name='task_create'),
     path('update/<int:pk>/', task_update, name='task_update'),
     path('delete/<int:pk>/', task_delete, name='task_delete'),
+    path('task/<int:pk>/', task_view, name='task_view'),
 ]
