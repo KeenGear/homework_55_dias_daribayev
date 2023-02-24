ce21442809fd964ffde7132ee0038f95158c6f0f
Dias Daribayev <fission.frames-0s@icloud.com>
2023-02-24 15:41:40 +0600

    For some reason previous db stopped working switched to another

--- source/app/settings.py
+++ source/app/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hello_dj',
+        'NAME': 'homework_52',
         'USER': 'postgres',
         'PASSWORD': 'Xamsters21',
         'HOST': 'localhost',
@@ -115,9 +115,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
